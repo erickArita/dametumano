595038475547ef577a1e2197648012183426f321
Erick Arita <erick.emao@gmail.com>
2021-01-13 11:23:01 -0600

    Updates

---
+++ 12-1dd4e28b1f838bb43f52.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{b0ys:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),o=a.n(t);n.default=()=>o.a.createElement("footer",null,"© ",(new Date).getFullYear(),", Club Dame Tu Mano")}}]);
+//# sourceMappingURL=12-1dd4e28b1f838bb43f52.js.map
