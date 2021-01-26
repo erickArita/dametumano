2f6b567746af95ca178b36217a935445b53b05db
Erick Arita <erick.emao@gmail.com>
2021-01-26 14:06:52 -0700

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@Erick-Arita`,
   },
-  flags: { PRESERVE_WEBPACK_CACHE: true },
+  // flags: { PRESERVE_WEBPACK_CACHE: true },
   pathPrefix: "/dametumano/",
   plugins: [
     `gatsby-plugin-sass`,
