5159919ab8c64de3465e946fa47973d76e539de7
Erick Arita <erick.emao@gmail.com>
2021-02-17 18:55:44 -0600

    prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@Erick-Arita`,
   },
   // flags: { PRESERVE_WEBPACK_CACHE: true },
-  pathPrefix: "/dametumano/",
+  pathPrefix: "/dametumano",
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
